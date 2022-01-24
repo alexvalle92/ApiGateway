449d6efb5e7d769d2c5837f1c51b0a75586cbd02
Alexandre Rafael Valle <a.rvalle1@hotmail.com>
2022-01-24 08:45:09 -0300

    Primeiros testes api Gateway

---
+++ ApiGateway.Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Cliente.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private static readonly string[] Clientes = new[]
+        {
+            "Alexandre", "Rafael", "Tamili", "Joao", "Gabriel"
+        };
+
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Clientes);
+        }
+    }
+}
---
+++ ApiGateway.Produto/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Produto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private static readonly string[] Produtos = new[]
+        {
+            "SSD", "Processador", "Memória", "Placa de vídeo", "Mouse"
+        };
+
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Produtos);
+        }
+    }
+}
